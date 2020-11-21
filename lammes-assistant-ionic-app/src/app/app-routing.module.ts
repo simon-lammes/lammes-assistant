0d197a3bd0ed929648b63b550719cd7795c14266
Simon Lammes <simon.lammes@cofinpro.de>
2020-11-21 22:28:30 +0100

    introduced tabs as a foundational navigation concept

--- lammes-assistant-ionic-app/src/app/app-routing.module.ts
+++ lammes-assistant-ionic-app/src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: '**',
+    redirectTo: 'tabs'
   },
+  {
+    path: 'notes',
+    loadChildren: () => import('./notes/notes.module').then( m => m.NotesPageModule)
+  }
 ];
 
 @NgModule({
---
+++ lammes-assistant-ionic-app/src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'notes',
+        loadChildren: () => import('../notes/notes.module').then(m => m.NotesPageModule)
+      },
+      {
+        path: '**',
+        redirectTo: 'notes'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
