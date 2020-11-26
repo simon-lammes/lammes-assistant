de80741afee4a446c569b99720257197befeb826
Simon Lammes <simon.lammes@cofinpro.de>
2020-11-26 22:05:23 +0100

    The user can now create new notes in the app.

--- lammes-assistant-ionic-app/src/app/notes/notes.page.ts
+++ lammes-assistant-ionic-app/src/app/notes/notes.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Note, NotesService} from './notes.service';
+import {CreateNoteData, Note, NotesService} from './notes.service';
 import {Observable} from 'rxjs';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-notes',
@@ -11,11 +12,37 @@ export class NotesPage implements OnInit {
   notes$: Observable<Note[]>;
 
   constructor(
-    private notesService: NotesService
+    private notesService: NotesService,
+    private alertController: AlertController
   ) { }
 
   ngOnInit() {
     this.notes$ = this.notesService.usersNotes$;
   }
 
+  async createNote() {
+    const alert = await this.alertController.create({
+      header: 'Create Note',
+      inputs: [
+        {
+          name: 'text',
+          type: 'text',
+          placeholder: 'Note text'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Save',
+          handler: (result: CreateNoteData) => {
+            this.notesService.createNote(result);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
--- lammes-assistant-ionic-app/src/app/notes/notes.service.ts
+++ lammes-assistant-ionic-app/src/app/notes/notes.service.ts
@@ -2,6 +2,13 @@ import {Injectable} from '@angular/core';
 import {Apollo, gql} from 'apollo-angular';
 import {map} from 'rxjs/operators';
 
+/**
+ * The data that is needed for creating a note.
+ */
+export interface CreateNoteData {
+  text: string;
+}
+
 export interface Note {
   text: string;
 }
@@ -14,16 +21,57 @@ const usersNotesQuery = gql`
     }
 `;
 
+const createNoteMutation = gql`
+    mutation CreateNote($text: String!) {
+        createNote(text: $text) {
+            text
+        }
+    }
+`;
+
 @Injectable({
   providedIn: 'root'
 })
 export class NotesService {
 
-  constructor(
-    private apollo: Apollo
-  ) { }
-
-  readonly usersNotes$ = this.apollo.watchQuery<{myNotes: Note[]}>({query: usersNotesQuery}).valueChanges.pipe(
+  readonly usersNotes$ = this.apollo.watchQuery<{ myNotes: Note[] }>({query: usersNotesQuery}).valueChanges.pipe(
     map(({data}) => data.myNotes)
   );
+
+  constructor(
+    private apollo: Apollo
+  ) {
+  }
+
+  /**
+   * This method does not only send a request to the server in order to create a new note but it also takes care of caching.
+   * This is one thing I love about GraphQL so far. We make sure that the query for fetching the users notes gets automatically updated
+   * without even refetching the query. Our client is intelligent enough to know that when we add a note,
+   * this note gets appended the users notes. If you want to know more about this concept, I recommend this article
+   * https://www.apollographql.com/docs/angular/features/cache-updates/
+   * and I highly recommend this video https://www.youtube.com/watch?v=zWhVAN4Tg6M
+   * We want the queries to update immediately even when the network is slow, thich is why we use work with an optimistic response.
+   * You can read more about optimistic responses here: https://www.apollographql.com/docs/angular/features/optimistic-ui/
+   */
+  async createNote(data: CreateNoteData) {
+    await this.apollo.mutate({
+      mutation: createNoteMutation,
+      variables: data,
+      optimisticResponse: {
+        __typename: 'Mutation',
+        createNote: {
+          __typename: 'Note',
+          text: data.text
+        }
+      },
+      update: (cache, mutationResult) => {
+        const cachedData = cache.readQuery({query: usersNotesQuery}) as { myNotes: Note[] };
+        const newNote = (mutationResult.data as { createNote: Note }).createNote;
+        const updatedCacheData = {
+          myNotes: [...cachedData.myNotes, newNote]
+        };
+        cache.writeQuery({query: usersNotesQuery, data: updatedCacheData});
+      }
+    }).toPromise();
+  }
 }
