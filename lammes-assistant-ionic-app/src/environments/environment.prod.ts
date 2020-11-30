422017e83febfecdcc541df5110c8409c8974cc5
Simon Lammes <simon.lammes@cofinpro.de>
2020-11-30 14:03:40 +0100

    [WIP] hosted frontend now uses our server hosted on DigitalOcean App Platform

--- lammes-assistant-ionic-app/src/environments/environment.prod.ts
+++ lammes-assistant-ionic-app/src/environments/environment.prod.ts
@@ -2,8 +2,7 @@ import {Environment} from './environment-interface';
 
 export const environment: Environment = {
   production: true,
-  // For Android debugging, we use these production settings. To connect the emulated android device to the computers localhost,
-  // we have to replace localhost with 10.0.2.2. Once the server is deployed in the internet, we should replace this value.
-  uriGraphQl: 'http://10.0.2.2:4000/',
+  // For production, we use our server instance deployed on DigtalOcean App Plattform.
+  uriGraphQl: 'https://lammes-assistant-5m7y3.ondigitalocean.app/api',
   networkRetryDelayMilliseconds: 3000
 };
