977910e018b552d8512df52161c5b4eafee55d22
Simon Lammes <simon.lammes@cofinpro.de>
2021-01-27 16:10:58 +0100

    feat(exercises): Reflect exercise filter in url
    When the user is at '/tabs/exercises', his exercise filter automatically updates the URL's query params.
    
    closes #27

--- lammes-assistant-ionic-app/src/app/exercises/exercises.page.ts
+++ lammes-assistant-ionic-app/src/app/exercises/exercises.page.ts
@@ -2,16 +2,18 @@ import {Component, OnInit} from '@angular/core';
 import {ModalController, PopoverController} from '@ionic/angular';
 import {SaveExerciseModalPage} from './save-exercise-modal/save-exercise-modal.page';
 import {Exercise, ExerciseFilter, ExercisesService} from './exercises.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {ExercisesPopoverComponent} from './exercises-popover/exercises-popover.component';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {distinctUntilChanged, filter, first, startWith, switchMap} from 'rxjs/operators';
 import {UsersService} from '../shared/services/users/users.service';
 import _ from 'lodash';
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 
 const maximumForUnsigned32Int = Validators.max(4_294_967_295);
 
+@UntilDestroy()
 @Component({
   selector: 'app-exercises',
   templateUrl: './exercises.page.html',
@@ -19,7 +21,7 @@ const maximumForUnsigned32Int = Validators.max(4_294_967_295);
 })
 export class ExercisesPage implements OnInit {
   filteredExercises$: Observable<Exercise[]>;
-  filter$: Observable<any>;
+  validFilter$: Observable<any>;
   filterForm: FormGroup;
 
   constructor(
@@ -28,8 +30,22 @@ export class ExercisesPage implements OnInit {
     private router: Router,
     private popoverController: PopoverController,
     private formBuilder: FormBuilder,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private activatedRoute: ActivatedRoute
   ) {
+  }
+
+  /**
+   * Removes all parts of the filter that have no effect.
+   */
+  private static trimFilter(exerciseFilter: ExerciseFilter) {
+    const trimmedFilter: ExerciseFilter = {
+      creatorIds: exerciseFilter.creatorIds?.length > 0 ? exerciseFilter.creatorIds : undefined,
+      labels: exerciseFilter.labels?.length > 0 ? exerciseFilter.labels : undefined,
+      languageCodes: exerciseFilter.languageCodes?.length > 0 ? exerciseFilter.languageCodes : undefined,
+      maximumCorrectStreak: exerciseFilter.maximumCorrectStreak ?? undefined
+    };
+    return trimmedFilter;
   }
 
   async ngOnInit(): Promise<void> {
@@ -40,14 +56,15 @@ export class ExercisesPage implements OnInit {
       languageCodes: this.formBuilder.control([]),
       maximumCorrectStreak: this.formBuilder.control(undefined, [maximumForUnsigned32Int])
     });
-    this.filter$ = this.filterForm.valueChanges.pipe(
+    this.validFilter$ = this.filterForm.valueChanges.pipe(
       startWith(this.filterForm.value as ExerciseFilter),
       distinctUntilChanged((x, y) => _.isEqual(x, y)),
       filter(() => this.filterForm.valid)
     );
-    this.filteredExercises$ = this.filter$.pipe(
+    this.filteredExercises$ = this.validFilter$.pipe(
       switchMap(filterValue => this.exercisesService.getFilteredExercises(filterValue))
     );
+    this.setupAutomaticUpdateOfUrlQueryParams();
   }
 
   async createExercise() {
@@ -59,12 +76,7 @@ export class ExercisesPage implements OnInit {
 
   async startStudying() {
     const exerciseFilter = this.filterForm.value as ExerciseFilter;
-    const trimmedFilter: ExerciseFilter = {
-      creatorIds: exerciseFilter.creatorIds?.length > 0 ? exerciseFilter.creatorIds : undefined,
-      labels: exerciseFilter.labels?.length > 0 ? exerciseFilter.labels : undefined,
-      languageCodes: exerciseFilter.languageCodes?.length > 0 ? exerciseFilter.languageCodes : undefined,
-      maximumCorrectStreak: exerciseFilter.maximumCorrectStreak ?? undefined
-    };
+    const trimmedFilter = ExercisesPage.trimFilter(exerciseFilter);
     await this.router.navigate(['tabs', 'exercises', 'study'], {queryParams: trimmedFilter});
   }
 
@@ -91,5 +103,22 @@ export class ExercisesPage implements OnInit {
       }
     });
     return await modal.present();
+  }
+
+  /**
+   * Makes sure the current filter is reflected in the url. Inspired by
+   * [GitHub](https://docs.github.com/en/github/managing-your-work-on-github/sharing-filters).
+   */
+  private setupAutomaticUpdateOfUrlQueryParams() {
+    this.validFilter$.pipe(untilDestroyed(this)).subscribe(async currentFilter => {
+      const trimmedFilter = ExercisesPage.trimFilter(currentFilter);
+      await this.router.navigate(
+        [],
+        {
+          relativeTo: this.activatedRoute,
+          queryParams: trimmedFilter,
+          queryParamsHandling: 'merge'
+        });
+    });
   }
 }
