simon-lammes/lammes-assistant

commit 4c36c384aa1f36f318a020fc3ab2515da7debd58

ux: Settings should remain when I logout
As a user, I want setting like language and theme
to remain when I logout so that I am not confused.

closes #41

lammes-assistant-ionic-app/src/app/shared/services/users/user.service.ts
@@ -1,9 +1,10 @@
 import {Injectable} from '@angular/core';
 import {Apollo, gql} from 'apollo-angular';
 import {Observable} from 'rxjs';
-import {distinctUntilChanged, filter, map, switchMap} from 'rxjs/operators';
+import {distinctUntilChanged, filter, map, startWith, switchMap, tap} from 'rxjs/operators';
 import {AuthenticationService} from '../authentication/authentication.service';
 import _ from 'lodash';
+import {Storage} from '@ionic/storage';
 
 export interface User {
   id: number;
@@ -45,6 +46,8 @@ const filteredUsersQuery = gql`
   ${userFragment}
 `;
 
+const mostCurrentUserIdKey = 'UserService_mostCurrentUserId';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -63,7 +66,13 @@ export class UserService {
         ${userFragment}
       `
     }).valueChanges),
-    map(({data}) => data.me)
+    map(({data}) => data.me),
+    startWith(null as User),
+    tap(async user => {
+      if (user) {
+        await this.storage.set(mostCurrentUserIdKey, `${user.id}`);
+      }
+    })
   );
 
   readonly myProfilePicture$ = this.currentUser$.pipe(
@@ -73,7 +82,8 @@ export class UserService {
 
   constructor(
     private apollo: Apollo,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private storage: Storage
   ) {
   }
 
@@ -86,15 +96,10 @@ export class UserService {
     );
   }
 
-  getCachedUserById(userId: number): User {
-    try {
-      return this.apollo.client.readFragment({
-        id: `User:${userId}`,
-        fragment: userFragment
-      });
-    } catch (e) {
-      return undefined;
-    }
+  async getMostCurrentUserId(): Promise<number> {
+    const stringValue = await this.storage.get(mostCurrentUserIdKey);
+    const numberValue = +stringValue;
+    return typeof numberValue === 'number' ? numberValue : undefined;
   }
 
   setProfilePicture(picture: PictureInput) {

lammes-assistant-ionic-app/src/app/app.component.ts
@@ -5,6 +5,7 @@ import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {SwUpdate} from '@angular/service-worker';
 import {TranslateService} from '@ngx-translate/core';
+import {SettingsService} from './shared/services/settings/settings.service';
 
 @Component({
   selector: 'app-root',
@@ -18,10 +19,13 @@ export class AppComponent {
     private statusBar: StatusBar,
     private updates: SwUpdate,
     private alertController: AlertController,
-    private translate: TranslateService
+    private settingsService: SettingsService,
+    private translateService: TranslateService
   ) {
     this.initializeApp();
     this.setupServiceWorkerListeners();
+    this.setupThemeListener();
+    this.setupLanguage();
   }
 
   initializeApp() {
@@ -39,14 +43,14 @@ export class AppComponent {
   private setupServiceWorkerListeners() {
     this.updates.available.subscribe(async () => {
       const alert = await this.alertController.create({
-        header: await this.translate.get('new-version').toPromise(),
-        message: await this.translate.get('messages.new-pwa-version').toPromise(),
+        header: await this.translateService.get('new-version').toPromise(),
+        message: await this.translateService.get('messages.new-pwa-version').toPromise(),
         buttons: [
           {
-            text: await this.translate.get('cancel').toPromise(),
+            text: await this.translateService.get('cancel').toPromise(),
             role: 'cancel'
           }, {
-            text: await this.translate.get('reload').toPromise(),
+            text: await this.translateService.get('reload').toPromise(),
             handler: () => {
               this.updates.activateUpdate().then(() => document.location.reload());
             }
@@ -54,6 +58,24 @@ export class AppComponent {
         ]
       });
       await alert.present();
+    });
+  }
+
+  private setupThemeListener() {
+    const prefersDarkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    this.settingsService.currentSettings$.subscribe(settings => {
+      const useDarkTheme = settings.theme === 'dark'
+        || (settings.theme === 'system' && prefersDarkQuery.matches);
+      document.body.classList.toggle('dark-theme', useDarkTheme);
+    });
+  }
+
+  private setupLanguage() {
+    this.translateService.setDefaultLang('en');
+    this.settingsService.currentSettings$.subscribe(settings => {
+      if (settings.preferredLanguageCode) {
+        this.translateService.use(settings.preferredLanguageCode);
+      }
     });
   }
 }

lammes-assistant-ionic-app/src/app/app.module.ts
@@ -14,8 +14,7 @@ import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
 import {IonicStorageModule} from '@ionic/storage';
 import {MarkdownModule} from 'ngx-markdown';
-import {SettingsService} from './shared/services/settings/settings.service';
-import {TranslateLoader, TranslateModule, TranslateService} from '@ngx-translate/core';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 @NgModule({
@@ -51,29 +50,6 @@ import {TranslateHttpLoader} from '@ngx-translate/http-loader';
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor(
-    private settingsService: SettingsService,
-    private translateService: TranslateService
-  ) {
-    this.setupThemeListener();
-    this.setupLanguage();
-  }
-
-  private setupThemeListener() {
-    const prefersDarkQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    this.settingsService.currentSettings$.subscribe(settings => {
-      const useDarkTheme = settings.theme === 'dark'
-        || (settings.theme === 'system' && prefersDarkQuery.matches);
-      document.body.classList.toggle('dark-theme', useDarkTheme);
-    });
-  }
-
-  private setupLanguage() {
-    this.translateService.setDefaultLang('en');
-    this.settingsService.currentSettings$.subscribe(settings => {
-      if (settings.preferredLanguageCode) {
-        this.translateService.use(settings.preferredLanguageCode);
-      }
-    });
+  constructor() {
   }
 }

lammes-assistant-ionic-app/src/app/shared/services/settings/settings.service.ts
@@ -96,7 +96,10 @@ export class SettingsService {
     } as Settings);
   }
 
-  private retrieveCachedSettings(user: User): Promise<Settings | undefined> {
+  private retrieveCachedSettings(user: { id: number }): Promise<Settings | undefined> {
+    if (!user) {
+      return undefined;
+    }
     const cacheKey = `user.${user.id}.settings`;
     return this.storage.get(cacheKey) as Promise<Settings>;
   }
@@ -112,11 +115,19 @@ export class SettingsService {
     ).toPromise();
   }
 
+  /**
+   * @param user null if no user is logged in
+   */
   private async getUsersSettings(user: User) {
     const cachedSettings = await this.retrieveCachedSettings(user);
     const isCacheFresh = cachedSettings?.settingsUpdatedTimestamp
       && new Date(cachedSettings.settingsUpdatedTimestamp).getTime() === new Date(user.settingsUpdatedTimestamp).getTime();
-    if (isCacheFresh) {
+    if (!user) {
+      // If no user is logged in, we see what the id of the LAST user logged in was
+      // and take the cached settings from him.
+      const mostCurrentUserId = await this.userService.getMostCurrentUserId();
+      return await this.retrieveCachedSettings({id: mostCurrentUserId});
+    } else if (isCacheFresh) {
       return cachedSettings;
     } else {
       const downloadLink = await this.getSettingsDownloadLink();

commit 142157609d5932b5975aa0b42e8665d3ed5065cf

fix: broken exercise page
which froze up because there was something
wrong with the currentUser subscription.

Bug was introduced in 4c36c384aa1f36f318a020fc3ab2515da7debd58

lammes-assistant-ionic-app/src/app/shared/services/users/user.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Apollo, gql} from 'apollo-angular';
 import {Observable} from 'rxjs';
-import {distinctUntilChanged, filter, map, startWith, switchMap, tap} from 'rxjs/operators';
+import {distinctUntilChanged, filter, map, shareReplay, startWith, switchMap, tap} from 'rxjs/operators';
 import {AuthenticationService} from '../authentication/authentication.service';
 import _ from 'lodash';
 import {Storage} from '@ionic/storage';
@@ -72,7 +72,8 @@ export class UserService {
       if (user) {
         await this.storage.set(mostCurrentUserIdKey, `${user.id}`);
       }
-    })
+    }),
+    shareReplay(1)
   );
 
   readonly myProfilePicture$ = this.currentUser$.pipe(